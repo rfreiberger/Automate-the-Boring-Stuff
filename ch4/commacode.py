1a1edffe3cad49f7c98bf7c17f0d57c2adb4b6e8
Robert Freiberger <rfreiberger@gmail.com>
2016-02-20 02:13:39 -0800

    Added file for project in Ch4

---
+++ ch4/commacode.py
@@ -0,0 +1,13 @@
+# Chapter 4 
+# Project 1
+# Comma Code
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+
+joinedSpam = ','.join(spam[0:-1])
+lastSpam = spam[-1]
+
+print(joinedSpam + ",and " + lastSpam)
+
+
+
