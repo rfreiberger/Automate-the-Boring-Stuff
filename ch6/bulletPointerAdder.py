b3470a3b8738af2bd872f7e5356be40ea0e8ce18
Robert Freiberger <rfreiberger@gmail.com>
2016-02-21 01:35:16 -0800

    New file

---
+++ ch6/bulletPointerAdder.py
@@ -0,0 +1,12 @@
+#! python3
+# bulletPointAdder.py - Adds Wikipedia bullet points to the start
+# of each line of text on the clipboard.
+
+import pyperclip
+text = pyperclip.paste()
+
+# TODO: Separate lines and add starts.
+
+pyperclip.copy(text)
+
+
