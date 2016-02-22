ef8df3c8d9d2e94738558584545355da5ee8380e
Robert Freiberger <rfreiberger@gmail.com>
2016-02-21 20:35:12 -0800

    added loop

--- ch6/bulletPointerAdder.py
+++ ch6/bulletPointerAdder.py
@@ -6,6 +6,12 @@ import pyperclip
 text = pyperclip.paste()
 
 # TODO: Separate lines and add starts.
+# Separate lines and add stars.
+
+lines = text.split('\n')
+for i in range(len(lines)): # loop through all indexes in the "lines" list
+    lines[i] = '* ' + lines[i] # add star to each string in "lines" list
+
 
 pyperclip.copy(text)
 
