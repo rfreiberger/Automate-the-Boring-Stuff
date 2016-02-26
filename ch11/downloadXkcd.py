4dc98df3c55fdeda2568d96d790b57c40e911de7
Robert Freiberger <rfreiberger@gmail.com>
2016-02-26 02:03:22 -0800

    xkcd program

--- ch11/downloadXkcd.py
+++ ch11/downloadXkcd.py
@@ -6,7 +6,7 @@ import requests, os, bs4
 
 url = 'http://xkcd.com'                   # starting url
 os.makedirs('xkcd', exist_ok=True)        # store comics in ./xkcd
-while not url.endswitch('#'):
+while not url.endswith('#'):
 
 	# Download the page.
 	print('Downloading page %s...' % url)
