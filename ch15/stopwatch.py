05075af1e8012827954a306987df80917ef44f1a
Robert Freiberger <rfreiberger@gmail.com>
2016-03-13 00:36:54 -0800

    Adding new files

---
+++ ch15/stopwatch.py
@@ -0,0 +1,14 @@
+#! python3
+
+# stopwatch.py - A simple stopwatch program. 
+
+import time
+
+# Display the program's instructions. 
+print('Press ENTER to begin. Afterwards, press ENTER to "click" the stopwatch. Press Ctrl-C to quit.')
+input()   # press Enter to begin
+print('Started.')
+startTime = time.time()
+lastTime = startTime
+lapNum = 1
+
